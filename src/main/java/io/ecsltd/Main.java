7c3d849789741616f50213fcb4d715371ae40833
etiti <christopherobinna27@gmail.com>
2024-06-24 23:47:49 +0200

    god

---
+++ src/main/java/io/ecsltd/Main.java
@@ -0,0 +1,9 @@
+package io.ecsltd;
+
+public class Main {
+    public static void main(String[] args) {
+        Test name = new Test("obinna", 29);
+
+        System.out.println(name.getName());
+    }
+}
